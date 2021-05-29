ad2b4982846da28023ec18af0d97dbcd3b9b1dd7
Aditya Gupta <iadityapansari@gmail.com>
2021-05-29 11:42:46 +0530

    added notifiaction when 25 minutes has passed

--- background.js
+++ background.js
@@ -7,7 +7,16 @@ chrome.alarms.onAlarm.addListener((alarm) => {
     chrome.storage.local.get(["timer", "isRunning"], (res) => {
       if (res.isRunning) {
         let timer = res.timer + 1;
-        chrome.storage.local.set({ timer });
+        let isRunning = true;
+        if (res.timer == 25 * 60) {
+          this.registration.showNotification("Chrome Timer Ext", {
+            body: `25 minutes has passed.`,
+            icon: "../clock.png",
+          });
+          timer = 0;
+          isRunning = false;
+        }
+        chrome.storage.local.set({ timer, isRunning });
       }
     });
   }
